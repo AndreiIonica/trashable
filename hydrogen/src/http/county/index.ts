756325cfef4bd0336a59853aff6bb93df7c3d068
AndreiIonica <40264929+AndreiIonica@users.noreply.github.com>
2021-06-21 16:59:18 +0300

    feat(hydrogen): finished county routes

--- hydrogen/src/http/county/index.ts
+++ hydrogen/src/http/county/index.ts
@@ -1,3 +1,23 @@
-export default function init(): void {
-	console.log('adsf');
+import fetch from 'isomorphic-unfetch';
+
+export interface County {
+	id: number;
+	name: string;
+	code: string;
+	// eslint-disable-next-line camelcase
+	created_at: string;
+	// eslint-disable-next-line camelcase
+	updated_at: string;
+}
+
+export async function GetAll(baseUrl = 'https://trashable-server.herokuapp.com'): Promise<County[]> {
+	const raw = await fetch(`${baseUrl}/api/0.1/county/`);
+	const types = (await raw.json()) as County[];
+	return types;
+}
+
+export async function GetOne(id: number, baseUrl = 'https://trashable-server.herokuapp.com'): Promise<County> {
+	const raw = await fetch(`${baseUrl}/api/0.1/county/${id}`);
+	const type = (await raw.json()) as County;
+	return type;
 }

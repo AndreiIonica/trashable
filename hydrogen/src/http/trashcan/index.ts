b9f59c63ece6d63f8e7280b787104915d145dbfc
AndreiIonica <40264929+AndreiIonica@users.noreply.github.com>
2021-06-23 18:06:12 +0300

    feat(hydrogen): started implementing trashcan routes

--- hydrogen/src/http/trashcan/index.ts
+++ hydrogen/src/http/trashcan/index.ts
@@ -1,3 +1,35 @@
-export default function init(): void {
-	console.log('adsf');
+import fetch from 'isomorphic-unfetch';
+
+export interface Trashcan {
+	id: number;
+	latitude: number;
+	longitude: number;
+	// eslint-disable-next-line camelcase
+	street_address: string;
+	altitude: number;
+	// eslint-disable-next-line camelcase
+	type_id: number;
+	// eslint-disable-next-line camelcase
+	city_id: number;
+	// eslint-disable-next-line camelcase
+	user_id: number;
+	clean: boolean | null;
+	// eslint-disable-next-line camelcase
+	photo_url: string | null;
+	// eslint-disable-next-line camelcase
+	created_at: string;
+	// eslint-disable-next-line camelcase
+	updated_at: string;
+}
+
+export async function GetAll(baseUrl = 'https://trashable-server.herokuapp.com'): Promise<Trashcan[]> {
+	const raw = await fetch(`${baseUrl}/api/0.1/trashcan`);
+	const trashcans = (await raw.json()) as Trashcan[];
+	return trashcans;
+}
+
+export async function GetOne(id: number, baseUrl = 'https://trashable-server.herokuapp.com'): Promise<Trashcan> {
+	const raw = await fetch(`${baseUrl}/api/0.1/trashcan/${id}`);
+	const trashcan = (await raw.json()) as Trashcan;
+	return trashcan;
 }

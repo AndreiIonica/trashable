3fe3f765404a5dafa7b01abc143575b92f3f10cd
AndreiIonica <40264929+AndreiIonica@users.noreply.github.com>
2021-09-20 21:20:50 +0300

    started work on button component

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -5,3 +5,18 @@
 		</span>
 	</button>
 </template>
+
+<style scoped>
+button {
+	background: var(--accent-color);
+	color: #ffffff;
+	display: flex;
+	flex-direction: row;
+	height: 3rem;
+	padding: 1rem 2rem;
+	justify-content: space-evenly;
+	align-items: center;
+	border-radius: 4rem;
+	border: 0;
+}
+</style>

780069940c854bf8b7a20127785d40a89cab1468
AndreiIonica <40264929+AndreiIonica@users.noreply.github.com>
2021-10-06 17:34:00 +0300

    frontend: recycling center icon on map

--- frontend/src/assets/leaflet/MarkerIcons.ts
+++ frontend/src/assets/leaflet/MarkerIcons.ts
@@ -1,16 +1,25 @@
-import { rawSVG as markerIconSvg } from '@/assets/raw/Marker';
-import markerColors from '@/assets/raw/MarkerColors';
-import type { DivIcon as IDivIcon } from 'leaflet';
 import { divIcon as DivIcon } from 'leaflet';
+
+import type { DivIcon as IDivIcon } from 'leaflet';
+
+import markerColors from '@/assets/raw/MarkerColors';
+import { rawSVG as markerIconSVG } from '@/assets/raw/Marker';
+import { rawSVG as userIconSVG } from '../raw/RecyclingCenterIcon';
 
 const MarkerIcons: IDivIcon[] = [];
 markerColors.forEach((c) => {
 	MarkerIcons.push(
 		DivIcon({
-			className: 'mark',
-			html: markerIconSvg.replace('#000000', c),
+			className: 'marker-normal',
+			html: markerIconSVG.replace('#000000', c),
 		}),
 	);
 });
+MarkerIcons.push(
+	DivIcon({
+		className: 'marker-recycling-center',
+		html: userIconSVG,
+	}),
+);
 
 export default MarkerIcons;
--- frontend/src/assets/raw/MarkerColors.ts
+++ frontend/src/assets/raw/MarkerColors.ts
@@ -1,4 +1,4 @@
 // On the backend id's start from 1
-const colors = ['', '#60ad5e', '#2AAD27', '#2A81CB', '#FFD326', '#000000'];
+const colors = ['', '#60ad5e', '#2AAD27', '#2A81CB', '#FFD326'];
 
 export default colors;
